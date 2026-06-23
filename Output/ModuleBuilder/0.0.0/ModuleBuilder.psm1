7ce84378fb8cd766fd5043845c2c9ac1c6c2e8c5
Doki1992 <mike58072@gmail.com>
2026-06-22 21:59:02 -0600

    Compile and upload artifact

--- Output/ModuleBuilder/0.0.0/ModuleBuilder.psm1
+++ Output/ModuleBuilder/0.0.0/ModuleBuilder.psm1
@@ -28,12 +28,14 @@ function Set-GitConnection {
         [ValidateSet("2022-11-28")]
         [string]$ApiVersion
     )
-    $encodedPAT = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($GitHubPat))
-    $GitHubPat, $encodedPAT, $Url, $ApiVersion
+    # $encodedPAT = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($GitHubPat))
+    $env = Get-EnvVars
+    $PAT = $global:GIT_HUB_PAT    
+    $Headers = @{
+        "Authorization" = "Bearer $PAT"
+    }
+   $Response = Invoke-RestMethod -Uri $Url -Method Get -Headers $Headers
+   $Response 
 }
-$env = Get-EnvVars
-echo $global:GIT_HUB_PAT
-$data = Set-GitConnection -GitHubPat mike
-echo $data
-#EndRegion '.\Public\Get-GitConnection.ps1' 15
+#EndRegion '.\Public\Get-GitConnection.ps1' 17
 
